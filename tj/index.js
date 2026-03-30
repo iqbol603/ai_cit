5c0d694496003c451bed4ce5a0ea0132db1a7a6d
iqbol603 <kholmurodovikbol@gmail.com>
2026-03-30 17:02:57 +0500

    first commit

--- tj/index.js
+++ tj/index.js
@@ -1,7 +1,7 @@
 // Функсия барои оғози тест (Донишсанҷӣ)
 function startQuiz() {
     // Азбаски мо дар реша ҳастем, ба папкаи tj мегузарем
-    window.location.href = "./tj/ai_quiz_tj/index.html";
+    window.location.href = "./tj/AAi_quiz_tj/index.html";
 }
 
 // Функсия барои хондани мақолаи мушаххас
